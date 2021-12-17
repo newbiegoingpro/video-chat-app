14eca6fcb4d49bed1a0d8ac12b11179eff4c6e51
Nikolay Shvaika <hiptoz@yandex.ru>
2021-12-18 01:34:07 +0300

    debugWS

--- src/server/socket.js
+++ src/server/socket.js
@@ -7,6 +7,6 @@ const options = {
     transports: ['websocket']
 };
 
-const socket = io('/', options);
+const socket = io(`ws//:https://lit-atoll-99067.herokuapp.com:${process.env.PORT}`, options);
 
 export default socket;
